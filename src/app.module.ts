55c2d6ff72536cf9b494b70deda2326b3adbbd96
prismaticoder <pjjsalam1999@gmail.com>
2024-07-20 09:59:11 +0100

    feat: add db connection logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [config] }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  port: parseInt(process.env.APP_PORT, 10) || 3000,
+  database: {
+    name: process.env.DB_NAME,
+    type: 'sqlite',
+  },
+});
