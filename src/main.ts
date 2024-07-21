95c33f1b358a6f630c732b541d8c99fbd9ee4638
prismaticoder <pjjsalam1999@gmail.com>
2024-07-21 15:52:42 +0100

    feat: add versioning to endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalInterceptors(new TransformInterceptor());
 
