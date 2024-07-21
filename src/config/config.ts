8e63b46a21619ace589e969ddc54f0bf4c73f468
prismaticoder <pjjsalam1999@gmail.com>
2024-07-21 16:56:23 +0100

    chore: update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,5 @@
 export default () => ({
   port: parseInt(process.env.APP_PORT, 10) || 3000,
-  database: {
-    name: process.env.DB_NAME,
-    type: 'sqlite',
-  },
   swapi: {
     baseUrl: 'https://swapi.dev/api',
   },
